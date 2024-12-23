36d67356676f21a49fce53ebe5effbed56907e0e
houmayan <alichy101@gmail.com>
2024-12-23 09:54:46 +0100

    higher order function added

--- src/app/modules/students/students.controller.ts
+++ src/app/modules/students/students.controller.ts
@@ -1,61 +1,46 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { HttpStatus } from 'http-status-ts';
 import sendResponse from '../../utility/sendResponse';
 import { StudentService } from './students.service';
 
-const getAStudent = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const { id } = req.params;
-    const result = await StudentService.getAStudentFromDb(id);
-    sendResponse(res, {
-      statusCode: HttpStatus.OK,
-      success: true,
-      message: 'Student is created successfully',
-      data: result,
-    });
-    // res.status(HttpStatus.OK).json({
-    //   success: true,
-    //   message: 'Students id deleted Successfully',
-    //   data: result,
-    // });
-  } catch (error) {
-    next(error);
-  }
+const cathAsync = (fn: RequestHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+  };
 };
-const getAllStudents = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const result = await StudentService.getStudentsFromDb();
-    res.status(200).json({
-      success: true,
-      message: 'Students retrived Successfully',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-const deleteAStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { id } = req.params;
-    const result = await StudentService.deleteStudentFromDb(id);
-    res.status(200).json({
-      success: true,
-      message: 'Student deleted',
-      data: result,
-    });
-  } catch (error) {
-    // console.log(error);
-    next(error);
-  }
-};
+
+const getAStudent = cathAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const result = await StudentService.getAStudentFromDb(id);
+  sendResponse(res, {
+    statusCode: HttpStatus.OK,
+    success: true,
+    message: 'Student is created successfully',
+    data: result,
+  });
+  // res.status(HttpStatus.OK).json({
+  //   success: true,
+  //   message: 'Students id deleted Successfully',
+  //   data: result,
+  // });
+});
+const getAllStudents = cathAsync(async (req, res, next) => {
+  const result = await StudentService.getStudentsFromDb();
+  res.status(200).json({
+    success: true,
+    message: 'Students retrived Successfully',
+    data: result,
+  });
+});
+const deleteAStudent = cathAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const result = await StudentService.deleteStudentFromDb(id);
+  res.status(200).json({
+    success: true,
+    message: 'Student deleted',
+    data: result,
+  });
+});
 
 export const StudentController = {
   getAStudent,

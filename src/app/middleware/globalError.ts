0026c1b73f54210d0fc307bbdd078131887911ff
houmayan <alichy101@gmail.com>
2025-02-03 23:12:19 +0100

    error handeling

--- src/app/middleware/globalError.ts
+++ src/app/middleware/globalError.ts
@@ -1,5 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import config from '../config';
+import { TErrorSource } from '../interface/errorTypes';
 
 const globalErrorhandle = (
   err: any,
@@ -7,12 +10,43 @@ const globalErrorhandle = (
   res: Response,
   next: NextFunction,
 ) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'something went wrong';
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'something went wrong';
+
+  const handleZodError = (err: ZodError) => {
+    const errorSource: TErrorSource = err.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue?.path[issue.path.length - 1],
+        message: issue.message,
+      };
+    });
+    const statusCode = 400;
+
+    return {
+      statusCode,
+      message: 'Zod validation Error',
+      errorSource,
+    };
+  };
+
+  let errorSource: TErrorSource = [
+    {
+      path: '',
+      message: 'Something Went Wrong',
+    },
+  ];
+  if (err instanceof ZodError) {
+    const simplifiedZodError = handleZodError(err);
+    statusCode = simplifiedZodError?.statusCode;
+    message = simplifiedZodError?.message;
+    errorSource = simplifiedZodError?.errorSource;
+  }
   return res.status(statusCode).json({
     success: false,
-    error: err,
+    // error: err,
     message,
+    errorSource,
+    stack: config.NODE_ENV === 'developement' ? err?.stack : null,
   });
 };
 
